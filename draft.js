11f0435336262fa2c13330d6b902f7dbb04150f9
Ademir Ramos <93046432+AdemirRamos@users.noreply.github.com>
2021-12-31 10:36:01 -0300

    Updating The JS Archive
    Just adding a few more code lines to the archive.

--- draft.js
+++ draft.js
@@ -1,4 +1,4 @@
-var caixa, caixa_2, caixa_3, caixa_4, caixa_5, caixa_6
+var caixa, caixa_2, caixa_3, caixa_4, caixa_5, caixa_6, area_1, area_2, area_3, area_4, area_5, button_1, button_2
 
 window.addEventListener('load', eventos)
 
@@ -26,12 +26,26 @@ function eventos() {
     caixa_6 = document.querySelector('div#dv6')
     caixa_6.addEventListener('mouseover', div_switch_6)
     caixa_6.addEventListener('mouseout', div_switch_return_6)
+
+    area_1 = document.getElementById('title_2')
+    area_2 = document.querySelector('h3#title_3')
+    area_3 = document.getElementById('cabeçalho')
+    area_4 = document.getElementById('title_1')
+    area_5 = document.querySelector('body#corpo')
+
+    button_1 = document.getElementById('b1')
+    button_1.addEventListener('click', colour_1)
+
+    button_2 = document.querySelector('input#b2')
+    button_2.addEventListener('click', colour_2)
 }
 
 function div_switch() {
     caixa.style.backgroundColor = 'burlywood'
     caixa.innerHTML = 'This is a DIV!'
     caixa.style.textAlign = 'center'
+    caixa.style.lineHeight = '200px'
+    caixa.style.fontSize = '18px'
     caixa.style.color = 'coral'
     caixa.style.textShadow = '1px 1px 1px black'
 }
@@ -44,7 +58,9 @@ function div_switch_return() {
 function div_switch_2() {
     caixa_2.style.backgroundColor = 'skyblue'
     caixa_2.innerHTML = 'This is another DIV!'
+    caixa_2.style.fontSize = '18px'
     caixa_2.style.textAlign = 'center'
+    caixa_2.style.lineHeight = '200px'
     caixa_2.style.color = 'coral'
     caixa_2.style.textShadow = '1px 1px 1px black'
 }
@@ -57,7 +73,9 @@ function div_switch_return_2() {
 function div_switch_3() {
     caixa_3.style.backgroundColor = 'cornflowerblue'
     caixa_3.innerHTML = 'Just one thing: D I V!'
+    caixa_3.style.fontSize = '18px'
     caixa_3.style.textAlign = 'center'
+    caixa_3.style.lineHeight = '200px'
     caixa_3.style.color = 'coral'
     caixa_3.style.textShadow = '1px 1px 1px black'
 }
@@ -70,7 +88,9 @@ function div_switch_return_3() {
 function div_switch_4() {
     caixa_4.style.backgroundColor = 'darkorchid'
     caixa_4.innerHTML = 'Call me DIV!'
+    caixa_4.style.fontSize = '18px'
     caixa_4.style.textAlign = 'center'
+    caixa_4.style.lineHeight = '200px'
     caixa_4.style.color = 'coral'
     caixa_4.style.textShadow = '1px 1px 1px black'
 }
@@ -83,7 +103,9 @@ function div_switch_return_4() {
 function div_switch_5() {
     caixa_5.style.backgroundColor = 'darkred'
     caixa_5.innerHTML = 'Another round of DIV!'
+    caixa_5.style.fontSize = '18px'
     caixa_5.style.textAlign = 'center'
+    caixa_5.style.lineHeight = '200px'
     caixa_5.style.color = 'coral'
     caixa_5.style.textShadow = '1px 1px 1px black'
 }
@@ -96,7 +118,9 @@ function div_switch_return_5() {
 function div_switch_6() {
     caixa_6.style.backgroundColor = 'darkturquoise'
     caixa_6.innerHTML = 'Once again: DIV!'
+    caixa_6.style.fontSize = '18px'
     caixa_6.style.textAlign = 'center'
+    caixa_6.style.lineHeight = '200px'
     caixa_6.style.color = 'coral'
     caixa_6.style.textShadow = '1px 1px 1px black'
 }
@@ -104,4 +128,20 @@ function div_switch_6() {
 function div_switch_return_6() {
     caixa_6.style.backgroundColor = 'darkturquoise'
     caixa_6.innerHTML = ''
+}
+
+function colour_1() {
+    area_1.style.backgroundColor = 'steelblue'
+    area_2.style.backgroundColor = 'steelblue'
+    area_3.style.backgroundColor = 'steelblue'
+    area_4.style.backgroundColor = 'steelblue'
+    area_5.style.backgroundColor = 'steelblue'
+}
+
+function colour_2() {
+    area_1.style.backgroundColor = 'darkslateblue'
+    area_2.style.backgroundColor = 'darkslateblue'
+    area_3.style.backgroundColor = 'darkslateblue'
+    area_4.style.backgroundColor = 'darkslateblue'
+    area_5.style.backgroundColor = 'darkslateblue'
 }
